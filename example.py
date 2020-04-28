9c47371468182e60de2c9bb3a571cc16c9c70708
Philipp Dufter <philipp.dufter@gmail.com>
2020-04-28 11:10:28 +0200

    add alignment visualization code

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
 from aligner import SentenceAligner
 
 model = SentenceAligner()
-model.get_word_aligns(["Sir Nils Olav III. was knighted by the norwegian king .",
+result = model.get_word_aligns(["Sir Nils Olav III. was knighted by the norwegian king .",
                        "Nils Olav der Dritte wurde vom norwegischen König zum Ritter geschlagen ."])
+print(result)
